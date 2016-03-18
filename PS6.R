35b5547b5322c09f69716342143ae6dd12f14e19
jmetz2131 <jacob.metz@wustl.edu>
2016-03-17 20:13:18 -0500

    Initial Commit/Installing Packages
    Here is the first commit. I am installing the packages necessary to set
    up a Primary Race S4 package.

---
+++ PS6.R
@@ -0,0 +1,11 @@
+###Problem Set 6
+##Jacob Metz
+##Using devtools to create a package about the Primary race
+
+install.packages("devtools")
+library("devtools")
+install.packages("roxygen2")
+library("roxygen2")
+#The above lines of code are installing the code necessary to help me 
+#update the help files and functions within the same document that I'll commit to the
+#master.
