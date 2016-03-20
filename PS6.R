cc10eee9eb74839ae5954a4686f2256f92936035
jmetz2131 <jacob.metz@wustl.edu>
2016-03-19 22:49:20 -0500

    Created the Initial primaryrace package
    I named the package primaryrace by using the create() function, which
    resulted in the creation of DESCRIPTION and the NAMESPACE files. I will
    now change those files to be more appropriate for this new package.

--- PS6.R
+++ PS6.R
@@ -9,3 +9,15 @@ library("roxygen2")
 #The above lines of code are installing the code necessary to help me 
 #update the help files and functions within the same document that I'll commit to the
 #master.
+
+getwd()
+setwd("/Users/jacobmetz/Desktop/New Class/PS6")
+create("primaryrace")
+#Here I am setting up my working directory to be specifically where my file will save
+#to GitHub. 
+#I created the package primaryrace, which created the folder in my working directory
+#containing a DESCRIPTION file, a NAMESPACE file, and a R folder that will contain
+#functions I write for the package
+
+
+
