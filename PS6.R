e808ea47180aaf5305a0386ccd683bd044801d86
jmetz2131 <jacob.metz@wustl.edu>
2016-03-20 18:53:47 -0500

    Changed DESCRIPTION and NAMESPACE
    I am redoing how I originally created this package and will go over
    this with the TA in more detail tomorrow. I think I have a better
    understanding of what I need to do in order to create a function that
    accounts for delegates remaining. I am still struggling with coming up
    with that function and will transferring my code to the namespace after
    using the appropriate syntax. I am still figuring out how to properly
    code these specific things.

--- PS6.R
+++ PS6.R
@@ -2,22 +2,21 @@
 ##Jacob Metz
 ##Using devtools to create a package about the Primary race
 
-install.packages("devtools")
-library("devtools")
-install.packages("roxygen2")
-library("roxygen2")
-#The above lines of code are installing the code necessary to help me 
-#update the help files and functions within the same document that I'll commit to the
-#master.
-
+## Load libraries and set working directory
+library(devtools)
+library(roxygen2)
 getwd()
 setwd("/Users/jacobmetz/Desktop/New Class/PS6")
-create("primaryrace")
+
 #Here I am setting up my working directory to be specifically where my file will save
 #to GitHub. 
 #I created the package primaryrace, which created the folder in my working directory
 #containing a DESCRIPTION file, a NAMESPACE file, and a R folder that will contain
 #functions I write for the package
 
-
+create("primaryrace")
+## This can be run many times as the code is updates
+current.code <- as.package("primaryrace")
+load_all(current.code)
+document(current.code)
 
--- primaryrace/R/createCandidate.R
+++ primaryrace/R/createCandidate.R
@@ -1,3 +1,25 @@
+#' A candidate's name, party, and delegate count.
+#' 
+#' Object of class \code{Candidate} as created by the \code{createCandidate} function
+#'
+#' 
+#' An object of the class `Candidate' has the following slots:
+#' \itemize{
+#' \item \code{name} The name of the presidential candidate
+#' \item \code{delegatesWon} The delegates the presidential candidate has already won.
+#' \item \code{party} The party of the presidential candidate
+#' \item \code{delegatesNeeded} The amount of delegates the presidential candidate still needs in order to
+#'                              win the nomination
+#' }
+#'
+#' @author Jacob Metz: \email{jacob.metz@@wustl.edu}
+#' @aliases primaryrace-class initialize,primaryrace-method calculate_delegates,primaryrace-method 
+#' @rdname createCandidate
+#' @export
+## The above code shows that I am the author of this specific package, shows what the alias is for this package
+## so it is easily searchable in help files, and also shows what the title of the help file will be with the
+## @rdname command.
+
 setClass(Class="Candidate",
          #Here I am using S4 to set up a new class Candidate to prepare for
          #creating the function createCandidate
@@ -6,38 +28,25 @@ setClass(Class="Candidate",
            delegatesWon="numeric",
            party="character",
            delegatesNeeded="numeric"
-           ), 
+         ), 
+         
          #The above slots dictate what an object must have in order to be considered
          #part of the Candidate class. It will include the candidate's name, 
          #the amount of delegates he or she was already won, the party of the
          #candidate, and the delegates the candidate still needs to win in order
          #to clinch the respective party nomination.
+         
          prototype = prototype(
            name="",
            delegatesWon=c(),
            party="",
            delegatesNeeded=c()
          )
+         
          #I will only be looking at candidates from the Democratic and 
          #Republican Party, which require 2,383 delegates and 1,237 delegates, 
          #respectively, in order to clinch the nomination.
-        ) 
-
-setGeneric("createCandidate",
-    function(object="Candidate"){
-        standardGeneric("createCandidate")
-           })
-#Here I am setting up the generic function createCandidate - which is part
-#of the class Candidate - 
+) 
 
 
-#The following are my tests of whether the object will be accepted given how I
-#set up the class Candidate.
-
-#This test works because each slot is appropriately filled
-new("Candidate", 
-    name="Jed Bartlet",
-    delegatesWon=456,
-    party="Democratic",
-    delegatesNeeded=1927)
 
